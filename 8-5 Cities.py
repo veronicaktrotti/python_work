3b62843b99ebc75a351f7d9b01c8d0f575415302
veronicaktrotti <veronicaktrotti@students.abtech.edu>
2025-03-09 20:18:38 -0400

    Add files via upload

---
+++ 8-5 Cities.py
@@ -0,0 +1,7 @@
+def describe_city(city_name, country='USA'):
+    print(city_name+" is in "+country)
+    
+
+describe_city('Chicago')
+describe_city('New York')
+describe_city('Cologne', 'Germany')
